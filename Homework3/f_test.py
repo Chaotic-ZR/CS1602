9a07e4d8bcffa0d619421ec9975141af70746bce
Chaotic <chaotic-zr@sjtu.edu.cn>
2023-12-06 00:33:26 +0800

    First Commit

---
+++ Homework3/f_test.py
@@ -0,0 +1,8 @@
+def check_number(num):
+    return "Positive" if num > 0 else "Non-positive"
+
+result = check_number(5)
+print(result)  # 输出：Positive
+
+result = check_number(-2)
+print(result)  # 输出：Non-positive
