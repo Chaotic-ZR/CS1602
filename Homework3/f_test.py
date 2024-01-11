fdd01980da489261898563c082fda8a85871cd90
Chaotic <chaotic-zr@sjtu.edu.cn>
2024-01-11 18:22:25 +0800

    End of CS1602!

--- Homework3/f_test.py
+++ Homework3/f_test.py
@@ -1,8 +1,13 @@
-def check_number(num):
-    return "Positive" if num > 0 else "Non-positive"
+# def check_number(num):
+#     return "Positive" if num > 0 else "Non-positive"
 
-result = check_number(5)
-print(result)  # 输出：Positive
+# result = check_number(5)
+# print(result)  # 输出：Positive
 
-result = check_number(-2)
-print(result)  # 输出：Non-positive
+# result = check_number(-2)
+# print(result)  # 输出：Non-positive
+
+a = 80
+b = 99.8
+c = a * 0.5 + b * 0.5
+print(int(c))
